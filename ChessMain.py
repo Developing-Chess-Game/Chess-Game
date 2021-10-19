7445fe18860fd6a7cceab568552fd4c2b99a2f78
Eduardo <edwardo16blak@gmail.com>
2021-10-19 19:31:26 -0400

    This create the LoadImg() method.

--- ChessMain.py
+++ ChessMain.py
@@ -10,3 +10,16 @@ DIMENTION = 8 #the board is 8x8
 SQ_SIZE = DIMENTION // HEIGHT
 MAX_FPS = 15
 IMAGES = {}
+
+"""
+This help us to load the images in a less expensive way than loading every image in the same moment that they
+are drawn in the screen. 
+"""
+#loading images into the IMAGES object
+def loadImg():
+    #we need the names of each pieces, lets create a list with all them together.
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bP", "wR", "wN", "wB", "wQ", "wK", "wP"]
+    for piece in pieces: #let's get through all the peaces name
+        # Our object <IMAGES> should look like this: 
+        # IMAGES = {"name_piece": imagen_of_the_piece}
+        IMAGES[piece] = p.transform.scale(p.image.load("img/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
