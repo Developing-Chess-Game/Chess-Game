5746e37325dae380a9cd417c6ce8e8242a1dad55
Eduardo <edwardo16blak@gmail.com>
2021-10-19 22:13:55 -0400

    This creates the move() class and some methods inside the move class itself.

--- ChessEngine.py
+++ ChessEngine.py
@@ -24,3 +24,39 @@ class GameState():
         
         self.whiteToMove = True
         self.moveLog = []
+
+
+"""
+This class is used to store information about the moves
+"""
+class Move():
+    #map keys to values
+    #keys: values
+    #we want to obtain the chess notation in every move
+    ranksToRows = {
+        "1": 7, "2": 6, "3": 5, "4": 4,
+        "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+
+    filesToCols = {
+        "a": 0, "b": 1, "c": 2, "d": 3,
+        "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        #this variables keep tracks of what piece have been moved, and the capture of what the pieces new
+        #location is
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+    
+    #THIS METHOD GET THE CHESS NOTATION OF THE FIRST POSITION AND THE LAST POSITION AND RETURN THE STRING
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+    
+    #THIS METHOD GET THE CHESS NOTATION BUT JUST OF ONE POSTITION AND RETURN THE STRING
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r] 
